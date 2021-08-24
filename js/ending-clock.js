c4986ac9a00b2f426fce8bfe117697112e2983c1
김성현 <kimsh4265@korea.ac.kr>
2021-08-24 19:14:54 +0900

    change jsfile little

--- js/ending-clock.js
+++ js/ending-clock.js
@@ -16,6 +16,9 @@ function getClock(){
     if(isvacation){
         clock[0].innerText = `개강(${nextdate.getFullYear()}-${String(nextdate.getMonth() + 1).padStart(2,"0")}-${String(nextdate.getDate()).padStart(2,"0")})까지`
     }
+    else{
+        clock[0].innerText = `종강(${nextdate.getFullYear()}-${String(nextdate.getMonth() + 1).padStart(2,"0")}-${String(nextdate.getDate()).padStart(2,"0")})까지`
+    }
     clock[1].innerText = `${days}일 ${hours}시간 ${minutes}분 ${seconds}초`;
 }
 
